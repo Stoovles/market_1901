834b60d3a045387fac52037696f33f79cef29b21
Ethan Grab <ethangrab@gmail.com>
2019-02-20 09:20:32 -0700

    Iteration 1 - finished

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require 'pry'
 
 class VendorTest < Minitest::Test
   def test_it_exists
@@ -16,6 +17,40 @@ class VendorTest < Minitest::Test
   def test_inventory_starts_as_empty_hash
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_equal ({}), vendor.inventory
+  end
+
+  def test_inventory_defaults_to_zero
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal 0, vendor.check_stock("Peaches")
+  end
+
+  def test_it_can_add_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("Peaches", 30)
+
+    expected = {"Peaches" => 30}
+    assert_equal expected, vendor.inventory
+
+    vendor.stock("Tomatoes", 12)
+
+    expected = {"Peaches"=>30, "Tomatoes"=>12}
+    assert_equal expected, vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("Peaches", 30)
+
+    assert_equal 30, vendor.check_stock("Peaches")
+  end
+
+  def test_it_sums_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("Peaches", 30)
+    vendor.stock("Peaches", 25)
+
+    assert_equal 55, vendor.check_stock("Peaches")
   end
 
 end
